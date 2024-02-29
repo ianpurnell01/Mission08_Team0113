97f612e9c826cfd40583485d59722eb6f6597ea1
ipurnell <ianpurnell01@gmail.com>
2024-02-28 21:18:23 -0700

    Updated to save data (& pull data hopefully)

--- Mission08_Team0113/Controllers/HomeController.cs
+++ Mission08_Team0113/Controllers/HomeController.cs
@@ -1,24 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission08_Team0113.Models;
 using System.Diagnostics;
+using System.Security.AccessControl;
 
 namespace Mission08_Team0113.Controllers
 {
     public class HomeController : Controller
     {
 
+        private IHabitsRepository _repo;
+
+        public HomeController(IHabitsRepository other)
+        {
+            _repo = other;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult AddTask()
         {
-            return View();
+            ViewBag.Categories = _repo.Categories.OrderBy(x => x.CategoryName).ToList();
+            return View("");
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Table t)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.AddTable(t);
+            }
+            return View("Confirmation");
         }
 
         public IActionResult Quadrant()
         {
+            //ViewBag.TaskName = _repo.Tables.FirstOrDefault(x => x.Task == "Brush Teeth");
             return View();
         }
     }
